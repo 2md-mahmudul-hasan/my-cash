eaabf66a19cdc7f2384f451c7408bad26a5868b4
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-02-13 07:28:56 +0600

    add balance

--- cash.js
+++ cash.js
@@ -1,11 +1,27 @@
-document.getElementById('login-btn').addEventListener('click', function(){
-const userEmail = document.getElementById('user-email').value;
-const userPassword = document.getElementById('user-password').value;
+document.getElementById('deposit-btn').addEventListener('click', function(){
+  console.log('btn click')
+  const depositField = document.getElementById('deposit-field');
+  const depositFieldValue = depositField.value;
+  const newDepositAmount = parseFloat(depositFieldValue);
+
+  //total deposit
+  const oldDeposit = document.getElementById('total-deposit')
+  const totalDepositText = oldDeposit.innerText;
+  const totalDepositAmount = parseFloat(totalDepositText)
+
+  const totalDeposit = totalDepositAmount + newDepositAmount;
+  oldDeposit.innerText = totalDeposit;
 
 
-if(userEmail === "murad@gamil.com" && userPassword === "123"){
-  window.location.href = "cash.html";
-}else{
-  console.log('invalid user')
-}
+  // net balance
+  const netBalance = document.getElementById('net-balance')
+  const netBalanceInnerText = netBalance.innerText;
+  const netBalanceAmount = parseFloat(netBalanceInnerText)
+
+  const totalBalance = netBalanceAmount + totalDeposit;
+  netBalance.innerText = totalBalance
+
+
+  depositField.value='';
+
 })
---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+const userEmail = document.getElementById('user-email').value;
+const userPassword = document.getElementById('user-password').value;
+
+
+if(userEmail === "murad@gamil.com" && userPassword === "123"){
+  window.location.href = "cash.html";
+}else{
+  console.log('invalid user')
+}
+})
+
+
